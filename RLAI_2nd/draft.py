51345eb97e5ed66d055cffa593353dc5013d9970
conference-supp <zkghhg@gmail.com>
2024-06-16 18:48:27 -0500

    Add nb for Ch4.

--- RLAI_2nd/draft.py
+++ RLAI_2nd/draft.py
@@ -1,53 +1,31 @@
-def plot_perf_metrics(ls_rewards, ls_opt_act_flgs, ls_param, param_prefix='epsilon', plt_lib='mlp'):
-    if plt_lib == 'mlp':
-        fig, axes = plt.subplots(2, 1, figsize=(10, 10))
-        
-        for i, eps in enumerate(ls_param):
-            axes[0].plot(np.mean(ls_rewards[i], axis=0), label='param = {}'.format(eps))
-            axes[1].plot(np.mean(ls_opt_act_flgs[i], axis=0), label='param = {}'.format(eps))
-        
-        axes[0].set_title('Average Reward')
-        axes[0].set_xlabel('Steps')
-        axes[0].set_ylabel('Average Reward')
-        axes[0].legend()
-        
-        axes[1].set_title('% Optimal Action')
-        axes[1].set_xlabel('Steps')
-        axes[1].set_ylabel('% Optimal Action')
-        axes[1].legend()
-        
-        plt.show()
-    else:
-        # Create subplots with 2 rows and 1 column
-        fig = make_subplots(rows=2, cols=1)
-
-        for i, eps in enumerate(ls_param):
-            
-            # Add traces to the first subplot for rewards
-            fig.add_trace(go.Scatter(x=np.arange(ls_rewards[0].shape[1]),
-                                    y=np.mean(ls_rewards[i], axis=0),
-                                    mode='lines',
-                                    name='{} = {}'.format(param_prefix, eps),
-                                    line=dict(color=COLOR_LIST[i])), row=1, col=1)
-
-            # Add traces to the second subplot for optimal action rate
-            fig.add_trace(go.Scatter(x=np.arange(ls_opt_act_flgs[0].shape[1]),
-                                    y=np.mean(ls_opt_act_flgs[i], axis=0),
-                                    mode='lines',
-                                    # name='param = {}'.format(eps),
-                                    line=dict(color=COLOR_LIST[i]),
-                                    showlegend=False),
-                                    row=2, col=1)
-        
-        # Add axis titles
-        fig.update_xaxes(title_text='Steps', row=1, col=1)
-        fig.update_yaxes(title_text='Average Reward', row=1, col=1)
-        fig.update_xaxes(title_text='Steps', row=2, col=1)
-        fig.update_yaxes(title_text='% Optimal Action', row=2, col=1)
-
-        # Update layout
-        fig.update_layout(height=600, width=800, title_text='Subplots')
-
-        # Show the figure
-        fig.show()
-        
+def state_val_update2(vnn):
+    def _ij(i, j):
+        return i*4+j
+    nr, nc = 4, 4
+    # Using the vnn values below to use the newly obtained v_{n+1} values.
+    for i, j in product(range(nr), range(nc)):
+        if (i==0 and j==0) or (i==nr-1 and j==nc-1):
+            vnn[_ij(i, j)] = 0
+        else:
+            # Corners
+            if i==0 and j==nc-1:
+                vnn[_ij(i, j)] = -1+sum([ga*vnn[_ij(i, j-1)], ga*vnn[_ij(i, j)], ga*vnn[_ij(i, j)], ga*vnn[_ij(i+1, j)]])/4
+            elif i==nr-1 and j==0:
+                vnn[_ij(i, j)] = -1+sum([ga*vnn[_ij(i, j)], ga*vnn[_ij(i, j+1)], ga*vnn[_ij(i-1, j)], ga*vnn[_ij(i, j)]])/4
+            # Boundaries
+            elif i==0:
+                vnn[_ij(i, j)] = -1+sum([ga*vnn[_ij(i, j-1)], ga*vnn[_ij(i, j+1)], ga*vnn[_ij(i, j)], ga*vnn[_ij(i+1, j)]])/4
+            elif i==nr-1:
+                vnn[_ij(i, j)] = -1+sum([ga*vnn[_ij(i, j-1)], ga*vnn[_ij(i, j+1)], ga*vnn[_ij(i-1, j)], ga*vnn[_ij(i, j)]])/4
+            elif j==0:
+                vnn[_ij(i, j)] = -1+sum([ga*vnn[_ij(i, j)], ga*vnn[_ij(i, j+1)], ga*vnn[_ij(i-1, j)], ga*vnn[_ij(i+1, j)]])/4
+            elif j==nc-1:
+                vnn[_ij(i, j)] = -1+sum([ga*vnn[_ij(i, j-1)], ga*vnn[_ij(i, j)], ga*vnn[_ij(i-1, j)], ga*vnn[_ij(i+1, j)]])/4
+            # Inner
+            else:
+                vnn[_ij(i, j)] = -1+sum([ga*vnn[_ij(i, j-1)], ga*vnn[_ij(i, j+1)], ga*vnn[_ij(i-1, j)], ga*vnn[_ij(i+1, j)]])/4
+                
+    vnn
+    
+    
+    \[(i[+\-0-9]*), (j[+\-0-9]*)\]
